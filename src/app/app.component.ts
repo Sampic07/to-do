934b5ffb0e1253bd9f7aedd288464c2623f0acdd
Abdusamat <sampic606@gmail.com>
2023-06-11 23:58:51 +0500

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ITodo } from 'src/interfaces';
+import { TodoService } from './shared/services';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'todo';
+export class AppComponent implements OnInit, OnDestroy {
+
+  title = 'Todo App';
+  todos: ITodo[] = [];
+
+  selectedTodo?: ITodo;
+
+  todo = "";
+
+  private todoSubscriptions: Subscription[] = [];
+
+  get totalTodo(): number { return this.todos.length }
+
+  constructor(private _todoSvc: TodoService) { }
+
+  ngOnInit(): void {
+    this.todoSubscriptions.push(this._todoSvc.getTodos().subscribe(todos => this.todos = [...todos]));
+  }
+
+  ngOnDestroy(): void {
+    this.todoSubscriptions.forEach(element => {
+      element.unsubscribe();
+    });
+  }
+
+  createTodo(): void {
+    if (!this.todo) return
+    const todo: ITodo = { name: this.todo };
+    this.todo = '';
+    this._todoSvc.createTodo(todo).subscribe(todo => { this.todos.push(todo) });
+  }
+
+  deleteTodo(todo: ITodo): void {
+    const index = this.todos.indexOf(todo);
+    this._todoSvc.deleteById(todo.id!).subscribe(id => this.todos.splice(index, 1));
+  }
+
+  clearAll(): void {
+    this._todoSvc.deleteAll().subscribe(isDeleted => {
+      if (isDeleted) this.todos = [];
+    })
+  }
+
 }
---
+++ src/app/shared/services/todo.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { ITodo } from 'src/interfaces';
+
+const TODOS = 'todos';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+
+  constructor() { }
+
+  createTodo(todo: ITodo): Observable<ITodo> {
+    const newTodo = { id: this._newId, name: todo.name }
+    this._addTodo(newTodo);
+    return of(newTodo);
+  }
+
+  getTodos(): Observable<ITodo[]> {
+    return of(this._todos);
+  }
+
+  deleteById(id: number): Observable<number> {
+    this._removeTodo(id);
+    return of(id);
+  }
+
+  deleteAll(): Observable<boolean> {
+    localStorage.removeItem(TODOS);
+    return of(true);
+  }
+
+  private get _newId(): number {
+    return this._todos.length > 0 ? ++this._todos[this._todos.length - 1].id! : 1;
+  }
+
+  private get _todos(): ITodo[] {
+    return JSON.parse(localStorage.getItem(TODOS) || '[]');
+  }
+
+  private _addTodo(todo: ITodo) {
+    const todos: ITodo[] = [...this._todos];
+    todos.push(todo);
+    localStorage.setItem(TODOS, JSON.stringify(todos));
+  }
+
+  private _removeTodo(id: number) {
+    const todos = this._todos.filter(t => t.id !== id)
+    localStorage.setItem(TODOS, JSON.stringify(todos));
+  }
+}
